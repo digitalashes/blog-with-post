0dc09ad9fb25da3b4c7e0fc6b8ef18627f9977a7
digitalashes <woop.woop.woop.wooop.woop.woop@gmail.com>
2018-03-06 22:01:59 +0200

    Add link to user.

--- project/apps/posts/admin.py
+++ project/apps/posts/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext as _
 
 from posts.models import Post
 
@@ -6,7 +10,7 @@ from posts.models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'title', 'author', 'status'
+        'id', 'title', 'author_link', 'status'
     )
     list_display_links = ('title',)
     readonly_fields = (
@@ -19,3 +23,10 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status',)
     list_select_related = ('author',)
     raw_id_fields = ('author',)
+
+    def author_link(self, obj):
+        url = reverse('admin:users_user_change', args=(obj.author_id,))
+        return mark_safe(f'<a href="{url}">{escape(obj.author)}</a>')
+
+    author_link.allow_tags = True
+    author_link.short_description = mark_safe(_('Author'))
