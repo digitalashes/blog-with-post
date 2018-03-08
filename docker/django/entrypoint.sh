5e05b65697ba43ec30d4bbdbd384758ad7258a67
digitalashes <woop.woop.woop.wooop.woop.woop@gmail.com>
2018-03-08 19:55:24 +0200

    Docker tuning.

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -2,5 +2,4 @@
 #migrate, create super and run server
 
 ./manage.py migrate --noinput
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | ./manage.py shell
 ./manage.py runserver 0.0.0.0:8000
