fba5592ce197e53290025b8178a915129e98e317
digitalashes <woop.woop.woop.wooop.woop.woop@gmail.com>
2018-03-08 18:25:55 +0200

    Fix permissions.

--- project/apps/comments/permissions.py
+++ project/apps/comments/permissions.py
@@ -1,16 +1,16 @@
 import datetime
 
 from django.conf import settings
+from pytz import UTC
 from rest_framework.permissions import BasePermission
 
 
 class TimeDeltaPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
+        now = UTC.localize(datetime.datetime.now())
         if request.method.lower() == 'patch':
             time_delta = settings.COMMENT_UPDATE_TIMEDELTA
-            now = datetime.datetime.now()
             return obj.modified > (now + time_delta)
         else:
             time_delta = settings.COMMENT_DELETE_TIMEDELTA
-            now = datetime.datetime.now()
             return obj.modified > (now + time_delta)
